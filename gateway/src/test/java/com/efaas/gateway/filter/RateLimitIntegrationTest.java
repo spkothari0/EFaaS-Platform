50181a12673350b2f37c260e3ec6e50595a6a321
Shreyas Kothari <spkothari0@gmail.com>
2026-03-30 01:42:44 -0400

    adding test cases

--- gateway/src/test/java/com/efaas/gateway/filter/RateLimitIntegrationTest.java
+++ gateway/src/test/java/com/efaas/gateway/filter/RateLimitIntegrationTest.java
@@ -22,7 +22,6 @@ import org.springframework.mock.web.server.MockServerWebExchange;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
